d4c646b0e8c8fc8d3dd5269211338a4417412d35
arthike <arthike36@gmail.com>
2023-06-10 11:46:48 +0200

    change

--- test.py
+++ test.py
@@ -1,20 +1,3 @@
-from selenium import webdriver
-
-# Kies de webbrowser die je wilt gebruiken en geef het pad naar de driver op
-browser = webdriver.Chrome('/pad/naar/chromedriver')
-
-# Open de zoekpagina van de webbrowser
-browser.get('https://www.google.com')
-
-# Zoek naar het zoekvak op de pagina en typ 'goedkope appels' in het zoekvak
-search_box = browser.find_element_by_name('q')
-search_box.send_keys('goedkope appels')
-
-# Zoek naar de zoekknop op de pagina en klik erop
-search_button = browser.find_element_by_xpath("//div[@class='FPdoLc tfB0Bf']//input[@name='btnK']")
-search_button.click()
-
-
 from datetime import datetime
 from django.utils import timezone
 from Login.models import Leerling, Sessie, Begeleider, User, Vak, Klas, Niveau, School
